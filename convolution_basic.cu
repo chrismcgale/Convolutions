867ec3699e7b2a43c7b1c4e656aadefb5103abcf
Christopher McGale <56483395+chrismcgale@users.noreply.github.com>
2024-03-23 16:41:41 -0400

    basic kernel

---
+++ convolution_basic.cu
@@ -0,0 +1,37 @@
+
+// Very basic, doesn't take advantage of constant memory caching of the filter or tiling N
+__global__ void convolution_2D_basic_kernel(float *N, float *F, float *P, int r, int width, int height) {
+    int outCol = blockIdx.x*blockDim.x + threadIdx.x;
+    int outRow = blockIdx.y*blockDim.y + threadIdx.y;
+    float pValue = 0.0f;
+    for (int fRow = 0; fRow < 2*r + 1; fRow++) {
+        for (int fCol; fCol < 2*r + 1; fCol++) {
+            int inRow = outRow - r + fRow;
+            int inCol = outCol - r + fCol;
+            if (inRow >= 0 && inRow < height && inCol >= 0 && inCol < width) {
+                pValue += F[fRow][fCol] * N[inRow*width + inCol];
+            }
+        }
+    }
+    P[outRow][outCol] = pValue;
+}
+
+__global__ void convolution_3D_basic_kernel(float *N, float *F, float *P, int r, int width, int length, int height) {
+    int outCol = blockIdx.x*blockDim.x + threadIdx.x;
+    int outRow = blockIdx.y*blockDim.y + threadIdx.y;
+    int outSlice = blockIdx.z*blockDim.y + threadIdx.z;
+    float pValue = 0.0f;
+    for (int fRow = 0; fRow < 2*r + 1; fRow++) {
+        for (int fCol; fCol < 2*r + 1; fCol++) {
+            for (int fSlice; fSlice < 2*r + 1; fSlice++) {
+                int inRow = outRow - r + fRow;
+                int inCol = outCol - r + fCol;
+                int inSlice = outSlice - r + fSlice;
+                if (inRow >= 0 && inRow < length && inCol >= 0 && inCol < width && inSlice >= 0 && inSlice < height) {
+                    pValue += F[fRow][fCol][fSlice] * N[inRow*length*width + inCol*width + slice];
+                }
+            }
+        }
+    }
+    P[outRow][outCol][outSlice] = pValue;
+}
